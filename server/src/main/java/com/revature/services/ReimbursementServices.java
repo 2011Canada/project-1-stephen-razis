f0e4d914fea8377dff98f6c89d71ab83cd2db256
Stephen Razis <srazis@yahoo.com>
2020-12-27 16:05:43 -0800

    [unstable laptop push] partial service implementation

--- server/src/main/java/com/revature/services/ReimbursementServices.java
+++ server/src/main/java/com/revature/services/ReimbursementServices.java
@@ -1,5 +1,59 @@
 package com.revature.services;
 
 public class ReimbursementServices {
+    IReimbursementDAO rd;
+    IUserDAO ud;
+    User activeUser;
 
+    public ReimbursementServices(User activeUser) {
+        rd = new ReimbursementDAOPostgres();
+        ud = new UserDAOPostgres();
+
+        this.activeUser = activeUser;
+    }
+
+    //TODO
+    public List<Reimbursement> GetAllReimbursements() {
+
+    }
+
+    //TODO
+    public List<Reimbursement> GetReimbursementsByAuthor(int authorId) {
+
+    }
+
+    //TODO
+    public Reimbursement GetReimbursementById(int reimbursementId) {
+
+    }
+
+    //TODO
+    public List<Reimbursement> GetReimbursementsByStatus(int statusId) {
+
+    }
+
+    //TODO
+    public List<Reimbursement> GetReimbursementsByType(int typeId) {
+
+    }
+
+    //TODO
+    public void AcceptReimbursementRequest(Reimbursement r) {
+
+    }
+
+    //TODO
+    public void RejectReimbursementRequest(Reimbursement r) {
+
+    }
+
+    //TODO
+    public void CreateReimbursementRequest(Reimbursement r) {
+
+    }
+
+    //TODO 
+    public void UpdateReimbursementRequest(Reimbursement r) {
+        
+    }
 }
--- server/src/main/java/com/revature/services/UserServices.java
+++ server/src/main/java/com/revature/services/UserServices.java
@@ -1,5 +1,59 @@
 package com.revature.services;
 
 public class UserServices {
+    IUserDAO ud;
+    User activeUser;
 
+    public UserServices(User activeUser) {
+        ud = new UserDAOPostgres();
+
+        this.activeUser = activeUser;
+    }
+
+    public User Login(String username, String password) {
+        User u = ud.FindUserByCredentials(username, password);
+        
+        return u;
+    }
+
+    public void UpdateFirstName(String firstName) {
+        activeUser.setFirstName(firstName);
+        ud.UpdateUser(activeUser);
+    }
+
+    public void UpdateLastName(String lastName) {
+        activeUser.setLastName(lastName);
+        ud.UpdateUser(activeUser);
+    }
+
+    public void UpdateEmail(String email) {
+        activeUser.setEmail(email);
+        ud.updateUser(activeUser);
+    }
+
+    public User GetUserById(int id) {
+        int permissionLevel = 2;
+        if (id == activeUser.getId() || (activeUser.getRoleId() <= permissionLevel && activeUser.getRoleId() > 0)) {
+            return ud.GetUserById(id);
+        }
+        return null;
+    }
+
+    public List<User> GetAllUsers() {
+        int permissionLevel = 2;
+        if (activeUser.getRoleId() <= permissionLevel && activeUser.getRoleId() > 0) {
+            return ud.GetAllUsers();
+        }
+        return null;
+    }
+
+    public List<User> GetUsersByRole(int roleId) {
+        int permissionLevel = 2;
+        if (roleId > 0 && roleId < 4 && (activeUser.getRoleId() <= permissionLevel && activeUser.getRoleId() > 0)) {
+            return ud.GetAllUsersByRole(roleId);
+        }
+        else {
+            return null;
+        }
+    }
 }
